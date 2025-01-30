26297f59a056dbb1a620be3086ec61dedb9d4a90
cabicar <mcabik881@gmail.com>
2025-01-30 11:02:44 +0100

    Mensi zmeny

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,14 @@
 public class Farm {
+    private int flowerAmount;
+    private int typeMax;
+    private int bigMax;
+    private int smolMax;
+    private double area;
 
-
-
+    public Farm() {
+        this.typeMax = 5;
+        this.bigMax = 10;
+        this.smolMax = 10;
+        this.area = 100;
+    }
 }
--- src/Flower.java
+++ src/Flower.java
@@ -1,15 +1,19 @@
+import java.util.Random;
+
 public abstract class Flower {
 
     protected String name;
     protected double price;
     protected double neededArea;
     protected double chanceOfGrowth;
+    protected boolean isPickable = false;
+    Random rd = new Random();
 
     public Flower(String name, double price, double neededArea, double chanceOfGrowth) {
         this.name = name;
         this.price = price;
         this.neededArea = neededArea;
-        this.chanceOfGrowth = chanceOfGrowth;
+        setChanceOfGrowth(chanceOfGrowth);
     }
 
     @Override
@@ -22,8 +26,66 @@ public abstract class Flower {
                 '}';
     }
 
+    public String grow(){
+        if(rd.nextInt(100)<=chanceOfGrowth){
+            price = price*3;
+            chanceOfGrowth = 0;
+            isPickable = true;
+            return "The \"F-F-F-F L O W E R, Destroyer of Gardens\" has grown successfully"; //enter boss music
+        }
+        return "The flower was too weak and has failed to grow :/";
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getNeededArea() {
+        return neededArea;
+    }
+
+    public void setNeededArea(double neededArea) {
+        this.neededArea = neededArea;
+    }
+
+    public double getChanceOfGrowth() {
+        return chanceOfGrowth;
+    }
+
+    public void setChanceOfGrowth(double chanceOfGrowth) {
+        if(chanceOfGrowth < 0){
+            this.chanceOfGrowth = 10;
+        }else if (chanceOfGrowth > 100){
+            this.chanceOfGrowth = 100;
+        }else {
+        this.chanceOfGrowth = chanceOfGrowth;
+        }
+    }
+
+    public boolean isPickable() {
+        return isPickable;
+    }
+
+    public void setPickable(boolean pickable) {
+        isPickable = pickable;
+    }
+
     public void water(){
-        chanceOfGrowth = this.chanceOfGrowth + 30;
+        setChanceOfGrowth(this.chanceOfGrowth + 30);
     }
 
 }
